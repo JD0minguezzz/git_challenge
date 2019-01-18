508faeff087c71ce788450a541e5a47b5356a9cf
Jorge Dominguez <jorge.dominguez@endava.com>
2019-01-18 15:03:06 -0500

    Added Main class

---
+++ src/git/challenge/Main.java
@@ -0,0 +1,26 @@
+package git.challenge;
+import java.util.Scanner;
+
+public class Main {
+
+    private static int op;
+    private static Scanner in = new Scanner(System.in);
+    private static Book nBook = new Book();
+
+    public static void main(String[] args) {
+
+        //Main menu...
+        while(op != 5){
+            System.out.println("********Endava Library********");
+            System.out.println("1. Add a new book.");
+            System.out.println("2. Search a book.");
+            System.out.println("3. Update a book.");
+            System.out.println("4. Delete a book.");
+            System.out.println("5. Exit.");
+            System.out.println();
+
+        }
+
+    }
+}
+
