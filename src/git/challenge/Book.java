d5ef4b6d6cfcd3d318ffd2ac4a9a03918c7984b9
Jorge Dominguez <jorge.dominguez@endava.com>
2019-01-18 11:22:31 -0500

    Added Book class with the instance variables (name, author, etc.) and the set/get methods

---
+++ src/git/challenge/Book.java
@@ -0,0 +1,70 @@
+package git.challenge;
+
+import java.util.Date;
+
+public class Book {
+
+    private String name;
+    private String author;
+    private String isbn;
+    private Date publishedDate;
+    private String language;
+    private String publisher;
+
+    public Book() {
+        this.name = name;
+        this.author = author;
+        this.isbn = isbn;
+        this.publishedDate = publishedDate;
+        this.language = language;
+        this.publisher = publisher;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+}
